535010242f404f0a83ae03027be1fc4c64da7e87
DarkKnife <serserpar801014@gmail.com>
2025-06-06 22:46:16 +0300

    Create Role

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,3 +11,15 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::middleware(['auth:api', 'role:admin'])->group(function () {
+    Route::get('/admin', fn() => response()->json(['msg' => 'Админка']));
+});
+
+Route::middleware(['auth:api', 'role:librarian'])->group(function () {
+    Route::get('/librarian', fn() => response()->json(['msg' => 'Библиотекарь']));
+});
+
+Route::middleware(['auth:api', 'role:user'])->group(function () {
+    Route::get('/user', fn() => response()->json(['msg' => 'Профиль пользователя']));
+});
